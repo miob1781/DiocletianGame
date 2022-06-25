0e8c26d90cce2281e6c46ab22aff5b56263680bf
Michael Oberst <mioberst@gmx.de>
2022-06-25 17:52:26 +0200

    create working game mechanism

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
+const styles = {
+    backgroundColor: {
+        red: "red",
+        green: "green",
+        blue: "blue",
+        yellow: "yellow",
+        orange: "orange",
+        purple: "purple"
+    },
+    color: {
+        red: "white",
+        green: "white",
+        blue: "white",
+        yellow: "white",
+        orange: "white",
+        purple: "white"
+    },
+    fontSize: {
+        1: "1rem",
+        2: "1.1rem",
+        3: "1.2rem",
+        4: "1.3rem"
+    }
+}
+
+
+
 class Game {
     constructor(board, players){
         this.board = board
         this.players = players // array with selected players
         this.playersRemaining = this.players // array with players still on the board
         this.playerOn = null
-        this.board.fields.forEach(field => field.game = this)
         this.hasOverflown = false
         this.gameOn = false
+        this.board.fields.forEach(field => field.game = this)
     }
 
     startGame(){
@@ -18,7 +45,7 @@ class Game {
     
     setIsOn(){
         this.playerOn.isOn = true
-        console.log(`It's ${this.playerOn}'s turn.`)
+        console.log(`It's ${this.playerOn.color}'s turn.`)
         if(this.playerOn.isComputer){
             this.computerMoves()
         }
@@ -73,55 +100,105 @@ class Player {
 
 class Board {
     constructor(players, size = 10, numThickBorders = 0){
-        const boardEl = document.getElementById("board")
-        console.log("The board: " + boardEl)
+        this.boardEl = document.getElementById("board")
+        this.size = size
         this.fields = []
+
         let k = 0
-        let index, row, col
-        for (let i = 0; i < size; i++){
-            for (let j = 0; j < size; j++){
-                col = i
-                row = j
-                index = k
+        for (let i = 1; i <= size; i++){
+            for (let j = 1; j <= size; j++){
+                const col = i
+                const row = j
+                const id = k
                 k++
-                
-                const field = new Field(index, row, col)
-                this.fields.push(field)
-                
+
+                const field = new Field(id, row, col, this)
                 const fieldEl = document.createElement("div")
-                fieldEl.className = "field"
+                fieldEl.className = `field row${row} col${col}`
+                fieldEl.id = "field" + id
                 fieldEl.style.gridArea = `${row} / ${col} / span 1 / span 1`
-                boardEl.appendChild(fieldEl)
+                this.boardEl.appendChild(fieldEl)
+                
+                fieldEl.addEventListener("click", () => {
+                    console.log("click on " + this.id)
+                    field.selectField()
+                })
+                
+                field.fieldEl = fieldEl
+                this.fields.push(field)
             }
         }
-        console.log("A new board has been created.")
+        this.fields.forEach(field => field.getNeighbors())
+        console.log(this)
     }
 }
 
 
 
 class Field {
-    constructor(index, row, col){
-        this.index = index
+    constructor(id, row, col, board){
+        this.id = id
         this.row = row
         this.col = col
         this.player = null
         this.value = 0
         this.neighbors = []
         this.game = null
+        this.board = board
+        this.fieldEl = null
     }
     
+    getNeighbors(){
+        const row = this.row
+        const col = this.col
+        const size = this.board.size
+        const fields = this.board.fields
+        let neighbor
+
+        if(col > 1){
+            neighbor = getField(fields, row, col-1)
+            this.neighbors.push(neighbor)
+        }
+        if(col < size){
+            neighbor = getField(fields, row, col+1)
+            this.neighbors.push(neighbor)           
+        }
+        if(row > 1){
+            neighbor = getField(fields, row-1, col)
+            this.neighbors.push(neighbor)
+        }
+        if(row < size){
+            neighbor = getField(fields, row+1, col)
+            this.neighbors.push(neighbor)
+        }
+    }
+
+    setField(player, value){
+        this.player = player
+        this.value = value
+        this.fieldEl.textContent = this.value
+        this.fieldEl.style.backgroundColor = styles.backgroundColor[this.player.color]
+        this.fieldEl.style.color = styles.color[this.player.color]
+        this.fieldEl.style.color = styles.fontSize[this.value.toFixed()]
+    }
+
     selectField(){
+        console.log(this.player.isOn)
         if(this.player.isOn){
+            console.log(`${this.player} clicked on ${this.id}`)
             this.player.isOn = false
-            this.field.increaseValue()
+            this.increaseValue()
             // Note: The next line is only executed when all fields are done with overflowing.
-            this.game.checkRemainingPlayers()             
+            // But this will need to be embedded in a promise if setTimeout() is used.
+            this.game.checkRemainingPlayers()
         }
     }
     
     increaseValue(){
         this.value++
+        console.log(this.value)
+        this.fieldEl.textContent = this.value
+        this.fieldEl.style.fontSize = styles.fontSize[this.value.toFixed()]
         if(this.value > this.neighbors.length){
             this.overflow()
         }
@@ -129,14 +206,21 @@ class Field {
     
     overflow(){
         this.value = 1
+        this.fieldEl.textContent = 1
+        this.fieldEl.style.fontSize = styles.fontSize["1"]
+        console.log(this.neighbors)
         this.neighbors.forEach(neighbor => {
-            neighbor.setPlayer(this.player)
+            if(neighbor.player === null || neighbor.player.color !== this.player.color){
+                neighbor.setPlayer(this.player)
+            }
             neighbor.increaseValue()
         })
     }
 
     setPlayer(player){
         this.player = player
+        this.fieldEl.style.backgroundColor = styles.backgroundColor[this.player.color]
+        this.fieldEl.style.color = styles.color[this.player.color]
     }
 }
 
@@ -151,10 +235,13 @@ const players = [red, blue]
 
 const board = new Board(players)
 
+board.fields[0].setField(red, 1)
+board.fields[44].setField(blue, 2)
+
 const game = new Game(board, players)
 
 game.startGame()
-
+console.log(game.playerOn.color)
 
 
 // helper functions
@@ -162,4 +249,8 @@ function selectRandomElement(arr){
     const index = Math.floor(arr.length * Math.random())
     const randomEl = arr[index]
     return randomEl
+}
+
+function getField(fields, row, col){
+    return fields.find(field => field.row === row && field.col === col)
 }
