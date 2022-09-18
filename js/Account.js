b4df18dc92c844b4c8ff8c33a11d362e26a84e66
Michael Oberst <mioberst@gmx.de>
2022-09-18 10:25:18 +0200

    add functions signup and login to class Account

--- js/Account.js
+++ js/Account.js
@@ -7,14 +7,60 @@ export class Account {
         username = null
         email = null
         createdGame = null
-        invitedGames = []
-        oldGames = []
+        invitedGames = null
+        oldGames = null
     }
     signup(){
-
-    }
-    login(){
-
+        const usernameEl = document.getElementById("signup-username")
+        const emailEl = document.getElementById("signup-email")
+        const passwordEl = document.getElementById("signup-password")
+        const submitEl = document.querySelector("#signup-form input[type='submit']")
+        
+        const username = usernameEl.value
+        const email = emailEl.value
+        const password = passwordEl.value
+        
+        const data = {username, email, password}
+        
+        submitEl.addEventListener("click", e => {
+            e.preventDefault()
+            axios.post(process.env.SERVER_URL, data)
+                .then(response => {
+                    const {id, username, email} = response.data
+                    this.id = id
+                    this.username = username
+                    this.email = email
+                })
+                .catch(err => console.log("error during signup: ", err))
+            })
+        }
+        
+        login(){
+            const usernameEl = document.getElementById("login-username")
+            const emailEl = document.getElementById("login-email")
+            const passwordEl = document.getElementById("login-password")
+            const submitEl = document.querySelector("#login-form input[type='submit']")
+            
+            const username = usernameEl.value
+            const email = emailEl.value
+            const password = passwordEl.value
+            
+            const data = {username, email, password}
+            
+            submitEl.addEventListener("click", e => {
+                e.preventDefault()
+                axios.post(process.env.SERVER_URL, data)
+                .then(response => {
+                    const {id, username, email, createdGame, invitedGames, oldGames} = response.data
+                    this.id = id
+                    this.username = username
+                    this.email = email
+                    this.createdGame = createdGame
+                    this.invitedGames = invitedGames
+                    this.oldGames = oldGames
+                })
+                .catch(err => console.log("error during login: ", err))
+        })
     }
     createGame(){
 
