e8dccb9e20f07f5800511147dd055693ab7edf4d
Michael Oberst <mioberst@gmx.de>
2022-09-21 19:43:49 +0200

    add forms and functionality to signup, login and logout

--- js/Account.js
+++ js/Account.js
@@ -1,55 +1,30 @@
-import {WebGame} from "./WebGame"
-import axios from "axios"
+import {WebGame} from "./WebGame.js"
+import {BASE_URL} from "./consts.js"
 
 export class Account {
     constructor(){
-        id = null
-        username = null
-        email = null
-        createdGame = null
-        invitedGames = null
-        oldGames = null
+        this.id = null
+        this.username = null
+        this.email = null
+        this.createdGame = null
+        this.invitedGames = null
+        this.oldGames = null
+        this.isLoggedIn = localStorage.getItem("authToken") ? true : false
     }
-    signup(){
-        const usernameEl = document.getElementById("signup-username")
-        const emailEl = document.getElementById("signup-email")
-        const passwordEl = document.getElementById("signup-password")
-        const submitEl = document.querySelector("#signup-form input[type='submit']")
-        
-        const username = usernameEl.value
-        const email = emailEl.value
-        const password = passwordEl.value
-        
-        const data = {username, email, password}
-        
-        submitEl.addEventListener("click", e => {
-            e.preventDefault()
-            axios.post(process.env.SERVER_URL, data)
-                .then(response => {
-                    const {id, username, email} = response.data
-                    this.id = id
-                    this.username = username
-                    this.email = email
-                })
-                .catch(err => console.log("error during signup: ", err))
-            })
-        }
-        
-        login(){
-            const usernameEl = document.getElementById("login-username")
-            const emailEl = document.getElementById("login-email")
-            const passwordEl = document.getElementById("login-password")
-            const submitEl = document.querySelector("#login-form input[type='submit']")
-            
-            const username = usernameEl.value
-            const email = emailEl.value
-            const password = passwordEl.value
-            
-            const data = {username, email, password}
-            
-            submitEl.addEventListener("click", e => {
-                e.preventDefault()
-                axios.post(process.env.SERVER_URL, data)
+
+    storeToken(token){
+        localStorage.setItem("authToken", token)
+    }
+
+    getHeaders(storedToken){
+        return {headers: {Authorization: `Bearer ${storedToken}`}}
+    }
+
+    authenticateUser(){
+        const storedToken = localStorage.getItem("authToken")
+        if(storedToken){
+            const headers = this.getHeaders(storedToken)
+            axios.get(BASE_URL + "/player/verify", headers)
                 .then(response => {
                     const {id, username, email, createdGame, invitedGames, oldGames} = response.data
                     this.id = id
@@ -58,14 +33,74 @@ export class Account {
                     this.createdGame = createdGame
                     this.invitedGames = invitedGames
                     this.oldGames = oldGames
+                    this.isLoggedIn = true
+                    
+                    console.log(this);
+                })
+                .catch(err => console.log("error during authentication: ", err))
+            }
+        }
+        
+        listenToSignup(){
+            const signupEl = document.getElementById("signup")
+            const usernameEl = document.getElementById("signup-username")
+            const emailEl = document.getElementById("signup-email")
+            const passwordEl = document.getElementById("signup-password")
+            const submitEl = signupEl.querySelector("input[type='submit']")
+            const openSignup = document.getElementById("open-signup")
+            
+            openSignup.addEventListener("click", () => {
+                signupEl.style.display = "block"
+            })
+            
+            submitEl.addEventListener("click", () => {
+                const username = usernameEl.value
+                const email = emailEl.value
+                const password = passwordEl.value
+                
+                const data = {username, email, password}
+                
+                axios.post(BASE_URL + "/player/signup", data)
+                .then(response => {
+                    this.storeToken(response.data.authToken)
+                    this.authenticateUser()
+                })
+                .catch(err => console.log("Error during signup: ", err))
+            })
+        }
+        
+        listenToLogin(){
+            const submitEl = document.querySelector("#login-form button")
+        
+            submitEl.addEventListener("click", () => {
+                const username = document.getElementById("login-username").value
+                const password = document.getElementById("login-password").value
+                
+                const data = {username, password}
+                
+                axios.post(BASE_URL + "/player/login", data)
+                .then(response => {
+                    this.storeToken(response.data.authToken)
+                    this.authenticateUser()
                 })
                 .catch(err => console.log("error during login: ", err))
+            })
+        }
+        
+        listentToLogout(){
+            const logoutEl = document.querySelector("#logout button")
+            
+            logoutEl.addEventListener("click", () => {
+            localStorage.removeItem("authToken")
+            this.isLoggedIn = false
         })
     }
-    createGame(){
+
+    listenToCreateGame(){
 
     }
-    acceptGame(){
-
+    
+    listenToAcceptGame(){
+        
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 import {Game} from "./Game.js"
+import {Account} from "./Account.js"
 
-// instantiating classes and starting a new game
+// instantiating classes, running listeners and starting a new game
+const account = new Account()
 const game = new Game()
+
+account.listenToSignup()
+account.listenToLogin()
+account.listentToLogout()
 
 game.getInput()
 game.createBoard()
