52a18bf8b270d5bc4ad2c074d76f5ce970f20e8c
Michael Oberst <mioberst@gmx.de>
2022-10-30 17:18:18 +0100

    fix bug: add missing bracket

--- js/Field.js
+++ js/Field.js
@@ -76,7 +76,7 @@ export class Field {
 
             if (this.game.webGameId) {
                 this.game.moveNum++
-                
+
                 if (
                     (!this.game.playerOn.isComputer && !this.game.playerOn.isExternalPlayer) ||
                     (this.game.playerIsCreator && this.player.isComputer)
@@ -92,48 +92,49 @@ export class Field {
                 this.game.getNextPlayer()
             }
         }
+    }
 
-        // increases the value of a field
-        increaseValue() {
-            if (!this.game.gameOn) {
-                return
-            }
-
-            this.value++
-            this.numEl.textContent = this.value
-            this.numEl.style.fontSize = styles.fontSize[this.value.toFixed()]
-
-            if (this.value > this.neighbors.length) {
-                this.overflow()
-            }
+    // increases the value of a field
+    increaseValue() {
+        if (!this.game.gameOn) {
+            return
         }
 
-        // selects the neighboring fields if the increased value of a field is greater than the number of neighbors
-        overflow() {
-            this.value = 1
-            this.numEl.textContent = 1
-            this.numEl.style.fontSize = styles.fontSize["1"]
+        this.value++
+        this.numEl.textContent = this.value
+        this.numEl.style.fontSize = styles.fontSize[this.value.toFixed()]
 
-            this.neighbors.forEach(neighbor => {
-                if (!neighbor.player || neighbor.player.color !== this.player.color) {
-                    let oldOwner
-
-                    if (neighbor.player) {
-                        oldOwner = neighbor.player
-                        neighbor.player.fields = neighbor.player.fields.filter(field => field.id !== neighbor.id)
-                    }
-
-                    neighbor.player = this.player
-                    neighbor.player.fields.push(neighbor)
-                    neighbor.fieldEl.style.backgroundColor = styles.backgroundColor[this.player.color]
-                    neighbor.fieldEl.style.color = styles.color[this.player.color]
-
-                    if (oldOwner) {
-                        this.game.checkRemainingPlayers(oldOwner)
-                    }
-                }
-
-                neighbor.increaseValue()
-            })
+        if (this.value > this.neighbors.length) {
+            this.overflow()
         }
     }
+
+    // selects the neighboring fields if the increased value of a field is greater than the number of neighbors
+    overflow() {
+        this.value = 1
+        this.numEl.textContent = 1
+        this.numEl.style.fontSize = styles.fontSize["1"]
+
+        this.neighbors.forEach(neighbor => {
+            if (!neighbor.player || neighbor.player.color !== this.player.color) {
+                let oldOwner
+
+                if (neighbor.player) {
+                    oldOwner = neighbor.player
+                    neighbor.player.fields = neighbor.player.fields.filter(field => field.id !== neighbor.id)
+                }
+
+                neighbor.player = this.player
+                neighbor.player.fields.push(neighbor)
+                neighbor.fieldEl.style.backgroundColor = styles.backgroundColor[this.player.color]
+                neighbor.fieldEl.style.color = styles.color[this.player.color]
+
+                if (oldOwner) {
+                    this.game.checkRemainingPlayers(oldOwner)
+                }
+            }
+
+            neighbor.increaseValue()
+        })
+    }
+}
