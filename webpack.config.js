b3fb1c1b01daafdae4090b2c1187285d41b7e382
Michael Oberst <mioberst@gmx.de>
2022-12-03 21:03:08 +0100

    change folder name from dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         main: path.resolve(__dirname, "./src/index.ts")
     },
     output: {
-        path: path.resolve(__dirname, "./dist"),
+        path: path.resolve(__dirname, "./docs"),
         filename: "[name].bundle.js",
     },
     module: {
